112f87d3990477d30195cc97401775b789c28a4e
JorgeAcebes <jorgea2004@gmail.com>
2024-10-09 18:41:46 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,6 @@
 chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ 
-    url: "https://www.renfe.com/es/es/cercanias/cercanias-madrid/horarios" 
-  }, (tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: fillAndSubmitForm
-    });
-  });
-});
-
-function fillAndSubmitForm() {
-  const iframe = document.querySelector("iframe#horariosCercanias");
-  if (iframe) {
-    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-    const origin = iframeDocument.querySelector("#IdOrigen");
-    const destination = iframeDocument.querySelector("#IdDestino");
-    const searchButton = iframeDocument.querySelector("#btnBusqueda");
-
-    if (origin && destination && searchButton) {
-      origin.value = "Cantoblanco Universidad";
-      destination.value = "Coslada";
-      searchButton.click();
-    }
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "open_url") {
+    chrome.tabs.create({ url: "https://calendar.google.com/calendar/u/0/r/tasks?tab=rc" });
   }
-}
+});
