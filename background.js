5c74b3d39111682871dbf8053017040dc14bf248
JorgeAcebes <jorgea2004@gmail.com>
2024-10-29 23:23:03 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-chrome.action.onClicked.addListener(() => {
 chrome.commands.onCommand.addListener((command) => {
   if (command === "open_url") {
     chrome.tabs.create({ url: "https://calendar.google.com/calendar/u/0/r/tasks?tab=rc" });
